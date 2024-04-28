0a031b8d75bb28a75470d0c4546853cc44761315
Brayan <brayanortegabco@gmail.com>
2024-04-27 19:38:34 -0500

    Creacion Metodo Intend En pagina principal para direccionarlo a CrearNota

--- app/src/main/java/co/edu/uniminuto/PaginaPrincipal.java
+++ app/src/main/java/co/edu/uniminuto/PaginaPrincipal.java
@@ -1,6 +1,9 @@
 package co.edu.uniminuto;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class PaginaPrincipal extends AppCompatActivity {
+private Button btnIngresoNotas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,22 @@ public class PaginaPrincipal extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
+        initObject();
+        btnIngresoNotas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {irPantallaNotas();
+
+            }
+        });
+    }
+    private void irPantallaNotas(){
+        Intent intent = new Intent(this, CrearNota.class);
+        startActivity(intent);
+    }
+
+    private void initObject(){
+        btnIngresoNotas = findViewById(R.id.btnIngresoNotas);
     }
 }
